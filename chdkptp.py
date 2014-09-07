e23007c641f9933c88131e78223fe95e4a48ba81
Johannes Baiter <johannes.baiter@gmail.com>
2014-09-08 00:45:59 +0200

    Proxy LuaRuntime so we have more control

--- chdkptp.py
+++ chdkptp.py
@@ -1,7 +1,12 @@
 import logging
+import numbers
+import os
 from collections import namedtuple
 
-from lupa import LuaRuntime
+from lupa import LuaRuntime, LuaError
+
+CHDKPTP_PATH = os.path.join(os.path.dirname(os.path.abspath(__file__)),
+                            'vendor', 'chdkptp')
 
 logger = logging.getLogger('pychdkptp.chdkptp')
 
@@ -11,51 +16,108 @@ DeviceInfo = namedtuple("DeviceInfo", ('status', 'model_name', 'bus_num',
 Message = namedtuple("Message", ('type', 'script_id', 'value'))
 
 
-def _get_lua_runtime():
-    runtime = LuaRuntime()
-    if not runtime.eval("type(jit) == 'table'"):
-        raise RuntimeError("lupa must be linked against Lua, not LuaJIT.\n"
-                           "Please install lupa with `--no-luajit`.")
-    # Load chdkptp modules
-    runtime.execute("""
-        require('chdkptp')
-        util = require('util')
-        util:import()
-        chdku = require('chdku')
-        exposure = require('exposure')
-        dng = require('dng')
-        prefs = require('prefs')
-    """)
+class PTPError(Exception):
+    def __init__(self, err_table):
+        Exception.__init__(self, "{0} (ptp_code: {1}".format(err_table.msg,
+                                                             err_table.ptp_rc))
+        self.ptp_code = err_table.ptp_rc
+        self.traceback = err_table.traceback
 
-    # Enable debug logging
-    runtime.execute("""
-        prefs._add('core_verbose', 'number', 'ptp core verbosity', 0,
-                   function(self) return corevar.get_verbose() end,
-                   function(self,val) corevar.set_verbose(val) end)
-        prefs._set('core_verbose', 2)
-        prefs._set('cli_verbose', 2)
-    """)
 
-    # Register loggers
-    runtime.execute("""
-        cli.infomsg = function(...)
-            python.eval(
-                'logger.info(\"\"\"' .. string.format(...) .. '\"\"\")'
-            )
-        end
+class LuaContext(object):
+    """ Proxy object around :class:`lupa.LuaRuntime` that wraps all Lua code
+        inside of `pcall` and raises proper Exceptions.
+    """
+    def _raise_exception(self, errval):
+        if isinstance(rval, (basestring, numbers.Number)):
+            raise LuaError(rval)
+        elif rval['etype'] == 'ptp':
+            raise PTPError(rval)
+        else:
+            raise LuaError(dict(rval))
 
-        cli.dbgmsg = function(...)
-            python.eval(
-                'logger.debug(\"\"\"' .. string.format(...) .. '\"\"\")'
-            )
-        end
-    """)
+    def eval(self, lua_code):
+        checked_code = "pcall(function() return {0} end)".format(lua_code)
+        status, rval = self._rt.eval(checked_code)
+        if not status:
+            self._raise_exception(rval)
+        return rval
 
-    # Create global connection object
-    runtime.execute("""
-        con = chdku.connection()
-    """)
-    return runtime
+    def execute(self, lua_code):
+        returns = lua_code.startswith("return")
+        checked_code = ("{0}pcall(function() return {1} end)"
+                        .format("return " if returns else "", lua_code))
+        status, rval = self._rt.execute(checked_code)
+        if not status:
+            self._raise_exception(rval)
+        return rval
+
+    def require(self, modulename):
+        return self._rt.require(modulename)
+
+    def table(self, *items, **kwargs):
+        return self._rt.table(*items, **kwargs)
+
+    @property
+    def globals(self):
+        return self._rt.globals()
+
+    def __init__(self):
+        self._rt = LuaRuntime()
+        if self.eval("type(jit) == 'table'"):
+            raise RuntimeError("lupa must be linked against Lua, not LuaJIT.\n"
+                            "Please install lupa with `--no-luajit`.")
+        self._setup_runtime()
+
+    def _setup_runtime(self):
+        # Set up module paths
+        self._rt.execute("""
+            CHDKPTP_PATH = python.eval("CHDKPTP_PATH")
+            package.path = CHDKPTP_PATH .. '/lua/?.lua;' .. package.path
+            package.cpath = CHDKPTP_PATH .. '/?.so;' .. package.path
+        """)
+
+        # Load chdkptp modules
+        self._rt.execute("""
+            require('chdkptp')
+            util = require('util')
+            util:import()
+            varsubst = require('varsubst')
+            chdku = require('chdku')
+            exposure = require('exposure')
+            dng = require('dng')
+            prefs = require('prefs')
+        """.format(CHDKPTP_PATH))
+
+        # Enable debug logging
+        self._rt.execute("""
+            prefs._add('core_verbose', 'number', 'ptp core verbosity', 0,
+                    function(self) return corevar.get_verbose() end,
+                    function(self,val) corevar.set_verbose(val) end)
+            prefs._set('core_verbose', 2)
+            prefs._set('cli_verbose', 2)
+        """)
+
+        # Register loggers
+        self._rt.execute("""
+            cli = {}
+            cli.infomsg = function(...)
+                python.eval(
+                    'logger.info(\"\"\"' .. string.format(...) .. '\"\"\")'
+                )
+            end
+
+            cli.dbgmsg = function(...)
+                python.eval(
+                    'logger.debug(\"\"\"' .. string.format(...) .. '\"\"\")'
+                )
+            end
+        """)
+
+        # Create global connection object
+        self._rt.execute("""
+            con = chdku.connection()
+        """)
 
 
 def list_devices():
