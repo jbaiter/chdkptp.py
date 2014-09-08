4935e1f0247a6b001dbb391c41485e060f64ffbf
Johannes Baiter <johannes.baiter@gmail.com>
2014-09-08 23:45:03 +0200

    Implement device enumeration, device initialization and remote lua execution

--- chdkptp.py
+++ chdkptp.py
@@ -10,9 +10,9 @@ CHDKPTP_PATH = os.path.join(os.path.dirname(os.path.abspath(__file__)),
 
 logger = logging.getLogger('pychdkptp.chdkptp')
 
-DeviceInfo = namedtuple("DeviceInfo", ('status', 'model_name', 'bus_num',
-                                       'device_num', 'vendor_id', 'product_id',
-                                       'serial_num'))
+DeviceInfo = namedtuple("DeviceInfo", ('model_name', 'bus_num', 'device_num',
+                                       'vendor_id', 'product_id',
+                                       'serial_num', 'chdk_api'))
 Message = namedtuple("Message", ('type', 'script_id', 'value'))
 
 
@@ -37,13 +37,19 @@ class LuaContext(object):
             raise LuaError(dict(errval))
 
     def eval(self, lua_code):
+        return self._rt.eval(lua_code)
+
+    def execute(self, lua_code):
+        return self._rt.execute(lua_code)
+
+    def peval(self, lua_code):
         checked_code = "pcall(function() return {0} end)".format(lua_code)
         status, rval = self._rt.eval(checked_code)
         if not status:
             self._raise_exception(rval)
         return rval
 
-    def execute(self, lua_code):
+    def pexecute(self, lua_code):
         returns = lua_code.startswith("return")
         checked_code = ("{0}pcall(function() return {1} end)"
                         .format("return " if returns else "", lua_code))
@@ -63,7 +69,7 @@ class LuaContext(object):
         return self._rt.globals()
 
     def __init__(self):
-        self._rt = LuaRuntime()
+        self._rt = LuaRuntime(unpack_returned_tuples=True)
         if self.eval("type(jit) == 'table'"):
             raise RuntimeError("lupa must be linked against Lua, not LuaJIT.\n"
                                "Please install lupa with `--no-luajit`.")
@@ -91,10 +97,6 @@ class LuaContext(object):
 
         # Enable debug logging
         self._rt.execute("""
-            prefs._add('core_verbose', 'number', 'ptp core verbosity', 0,
-                    function(self) return corevar.get_verbose() end,
-                    function(self,val) corevar.set_verbose(val) end)
-            prefs._set('core_verbose', 2)
             prefs._set('cli_verbose', 2)
         """)
 
@@ -126,25 +128,33 @@ def list_devices():
     :return:  All connected PTP devices
     :rtype:   List of `DeviceInfo` named tuples
     """
-    raise NotImplementedError
-
-
-def find_devices(bus_num=None, device_num=None, serial_num=None,
-                 product_id=None):
-    """ Find PTP devices on the USB bus.
-
-    :param bus_num:     Number of USB bus
-    :type bus_num:      int
-    :param device_num:  Number of USB device on bus
-    :type device_num:   int
-    :param serial_num:  Serial number of USB device
-    :type serial_num:   str/unicode
-    :param product_id:  USB product ID
-    :type product_id:   int
-    :return:            All matching PTP devices
-    :rtype:             List of `DeviceInfo` named tuples
-    """
-    raise NotImplementedError
+    lua = LuaContext()
+    devices = lua.execute("""
+    local info = {}
+    local devs = chdk.list_usb_devices()
+    for i, desc in ipairs(devs) do
+        local lcon = chdku.connection(desc)
+        lcon:connect()
+        table.insert(info, {
+            model_name = lcon.ptpdev.model,
+            bus_num = lcon.condev.bus,
+            device_num = lcon.condev.dev,
+            vendor_id = lcon.condev.vendor_id,
+            product_id = lcon.condev.product_id,
+            serial_num = lcon.ptpdev.serial_number,
+            chdk_api = lcon.apiver,
+        })
+        lcon:disconnect()
+    end
+    return info;
+    """)
+    infos = []
+    for dev_info in devices.values():
+        dev_info = dict(dev_info)
+        dev_info['chdk_api'] = (dev_info['chdk_api'].MAJOR,
+                                dev_info['chdk_api'].MINOR)
+        infos.append(DeviceInfo(**dev_info))
+    return infos
 
 
 class ChdkDevice(object):
@@ -154,8 +164,18 @@ class ChdkDevice(object):
         :param device_info:   Information about device to connect to
         :type device_info:    :class:`DeviceInfo`
         """
+        self.info = device_info
         self._lua = LuaContext()
-        raise NotImplementedError
+        self._lua.globals.devspec = self.info._asdict()
+        self._lua.execute("""
+        con = chdku.connection({bus = devspec.bus_num,
+                                dev = devspec.device_num})
+        con:connect()
+        """)
+
+    @property
+    def is_connected(self):
+        return self._lua.eval("con:is_connected()")
 
     def get_messages(self):
         """ Get all messages from device buffer
@@ -175,18 +195,45 @@ class ChdkDevice(object):
         # putm
         raise NotImplementedError
 
-    def lua_execute(self, lua_code, wait=True):
+    def lua_execute(self, lua_code, wait=True, do_return=True):
         """ Execute Lua code on the device.
 
         :param lua_code:    Lua code to execute
         :type lua_code:     str/unicode
         :param wait:        Block until code has finished executing
         :type wait:         bool
-        :return:            Return value of lua code, only if `wait=False`
+        :do_return:         Return value of lua code, only if `wait=True`
         :rtype:             bool/int/unicode/dict/tuple
         """
-        # lua
-        raise NotImplementedError
+        if not wait:
+            self._lua.execute("con:exec('{0}'").format(lua_code)
+            return None
+        # NOTE: Because of the frequency of curly braces, we prefer old-style
+        # string formatting in this case, since this saves us quite a bit of
+        # escaping
+        lua_rvals, msgs = self._lua.execute("""
+        local rvals = {}
+        local msgs = {}
+        con:execwait('%s', {rets=rvals, msgs=msgs})
+        return {rvals, msgs}
+        """ % lua_code).values()
+        if not do_return:
+            return None
+        return_values = []
+        for rv in lua_rvals.values():
+            # scalar
+            if rv.subtype != 'table':
+                return_values.append(rv.value)
+                continue
+            # to dict
+            parsed_val = self._lua.eval(rv.value)
+            parsed_val = dict(parsed_val)
+            # to tuple
+            if all(x.isdigit() for x in parsed_val):
+                parsed_val = tuple(parsed_val.values())
+            return_values.append(parsed_val)
+        return (tuple(return_values)
+                if len(return_values) > 1 else return_values[0])
 
     def kill_scripts(self, flush=True):
         """ Terminate any running script on the device.
