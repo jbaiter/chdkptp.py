e31e383f04cd5eedee0f2cb956d427081981dd7c
Johannes Baiter <johannes.baiter@gmail.com>
2014-09-09 22:02:26 +0200

    Add test script

---
+++ test.py
@@ -0,0 +1,44 @@
+import logging
+import os
+import tempfile
+
+import chdkptp
+
+
+logging.basicConfig(level=logging.DEBUG)
+
+tmp_dir = tempfile.mkdtemp()
+dev = chdkptp.ChdkDevice(chdkptp.list_devices()[0])
+
+print "Test files can be found under {0}".format(tmp_dir)
+
+print "Checking connectivity"
+assert dev.is_connected
+
+print "Checking mode switch"
+if dev.mode == 'record':
+    dev.switch_mode('play')
+    assert dev.mode == 'play'
+dev.switch_mode('record')
+assert dev.mode == 'record'
+
+print "Checking streaming JPEG capture"
+for i in xrange(5):
+    fpath = os.path.join(tmp_dir, "stream_{0:02}.jpg".format(i))
+    imgdata = dev.shoot()
+    with open(fpath, 'wb') as fp:
+        fp.write(imgdata)
+
+print "Checking streaming DNG capture"
+for i in xrange(5):
+    fpath = os.path.join(tmp_dir, "stream_{0:02}.dng".format(i))
+    imgdata = dev.shoot(dng=True)
+    with open(fpath, 'wb') as fp:
+        fp.write(imgdata)
+
+print "Checking downloading JPEG capture"
+for i in xrange(5):
+    fpath = os.path.join(tmp_dir, "download_{0:02}.jpg".format(i))
+    imgdata = dev.shoot(stream=False, download_after=True, remove_after=True)
+    with open(fpath, 'wb') as fp:
+        fp.write(imgdata)
